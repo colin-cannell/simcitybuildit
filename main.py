2a99a72355d814be9e9a7f02a5fd9c6dbbcb011b
colincannell <cjcannell35@gmail.com>
2025-03-05 17:01:29 -0500

    working on random function

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import numpy as np
-import pygame
-import sys
+import random
 
 class Road:
   def __init__(self):
@@ -139,9 +138,80 @@ class Grid:
                     return True
         return False
     
-# step 1 create a population of random city layouts
-def random_layout():
+residential = Residential("Residential Zone", 1, 2, 2, 1836)
+
+building_supplies = Store("Building Supplies", 2, 2, 2)
+hardware_store = Store("Hardware Store", 3, 2, 2)
+farmers_market = Store("Farmers Market", 4, 2, 2)
+furniture_store = Store("Furniture Store", 5, 2, 2)
+gardening_supplies = Store("Gardening Supplies", 6, 2, 2)
+donut_shop = Store("Donut Shop", 7, 2, 2)
+fashion_store = Store("Fashion Store", 8, 2, 2)
+fast_food = Store("Fast Food Restaurant", 9, 2, 2)
+home_appliances = Store("Home Appliances", 10, 2, 2)
+sports_shop = Store("Sports Shop", 11, 2, 2)
+toy_shop = Store("Toy Shop", 12, 2, 2)
+restoration_bureau = Store("Bureau of Restoration", 13, 2, 2)
+country_store = Store("Country Store", 14, 2, 2)
+desert_shop = Store("Desert Shop", 15, 2, 2)
+
+
+high_tech_factory = Factory("High Tech Factory", 16, 2, 2, (6, 6))
+
+nuclear_plant = Power("Nuclear Power Plant", 17, 4, 3, 60, (12, 12))
+water_station = Water("Water Pumping Station", 18, 2, 2, 55)
+sewage_plant = Sewage("Deluxe Sewage Plant", 19, 3, 2, 55, (0, 0))
+recycling_center = Waste("Recycling Center", 20, 3, 2, 50, (0, 0))
+
+fire_station = Fire("Deluxe Fire Station", 21, 4, 2, (22, 22))
+police_station = Police("Police Precinct", 22, 4, 2, (22, 22))
+hospital = Health("Hospital", 23, 4, 2, (24, 24))
+
+city_storage = Building("City Storage", 24, 3, 2)
+city_hall = Building("City Hall", 25, 3, 2)
+clock_tower = Building("Clock Tower", 26, 2, 2)
+town_hall = Building("Town Hall", 27, 3, 2)
+mayors_mansion = Building("Mayor's Mansion", 28, 3, 3)
+
+# Parks
+jogging_path = Specialization("Jogging Path", 29, 2, 2, 30, 8)
+university_cafe = Specialization("University Park Cafeteria", 30, 2, 2, 25, 10)
+reflection_pool = Specialization("Reflection Pool Park", 31, 2, 1, 20, 8)
+peaceful_park = Specialization("Peaceful Park", 32, 2, 1, 25, 10)
+urban_plaza = Specialization("Urban Plaza", 33, 2, 1, 20, 8)
+sculpture_garden = Specialization("Sculpture Garden", 34, 2, 1, 30, 8)
+tree_row = Specialization("Row of Trees", 35, 2, 1, 30, 12)
+casino_park = Specialization("Casino City Park", 36, 2, 2, 15, 10)
+soccer_field = Specialization("Soccer Field", 37, 2, 2, 25, 8)
+baseball_field = Specialization("Baseball Field", 38, 2, 2, 25, 8)
+swimming_pool = Specialization("Swimming Pool", 39, 4, 2, 15, 14)
+golf_course = Specialization("Golf Course", 40, 4, 6, 15, 16)
+
+# specialization
+surfer_beach = Specialization("Surfer Beach", 42, 2, 1, 10, 12)
+cozy_cottages = Specialization("Cozy Cottages", 44, 2, 1, 20, 12)
+
+# entertainment
+entertainment_hq = Specialization("Entertainment HQ", 43, 2, 2, 20, 8)
+
+# Gambling
+gambling_hq = Specialization("Gambling HQ", 45, 2, 2, 20, 8)
+
+
+# step 1 create a population of random city layout
+grid = 24
+buildings = [fire_station, police_station, hospital]
+
+# add roads to the layout
+def add_roads(layout):
     pass
+
+# random layout of buildings but they need to be connected by roads and in the area of police coverage
+def random_layout(buildings, size):
+    grid = [[random.choice(buildings) for _ in range(size)] for _ in range(size)]
+    layout = add_roads(grid)
+    return layout
+
 
 # step 2 sort population of layouts by fitness score
 def population_score(layout):
@@ -152,6 +222,9 @@ def population_score(layout):
                 total_residential += 1
     population = (total_residential // 4) * 1836
     return population
+
+def sort_layouts(layouts):
+    return sorted(layouts)
 
 # apply a cross over function
 def crossover():
